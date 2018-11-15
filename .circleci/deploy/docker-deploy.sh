92ed1700d80f5493964cdb80a1d1f0f1bb2250d8
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-15 22:14:41 +0000

    Update deply script

--- .circleci/deploy/docker-deploy.sh
+++ .circleci/deploy/docker-deploy.sh
@@ -5,4 +5,3 @@ set -e
 scp docker-compose.production.yml root@${PRODUCTION_SERVER}:~/stats-hub
 ssh root@${PRODUCTION_SERVER} "docker-compose -f ~/stats-hub/docker-compose.production.yml pull web && \
 docker-compose -f ~/stats-hub/docker-compose.production.yml up -d && \
-docker restart nginxproxy_nginx-proxy_1"
