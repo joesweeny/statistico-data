83333b34377f7f09ae9dfef1d8084351bba3ab1c
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-15 20:05:26 +0000

    Another fix to deploy script again

--- .circleci/deploy/docker-deploy.sh
+++ .circleci/deploy/docker-deploy.sh
@@ -3,5 +3,4 @@
 set -e
 
 scp docker-compose.production.yml root@${PRODUCTION_SERVER}:~/production
-ssh root@${PRODUCTION_SERVER} "docker-compose -f ./production/docker-compose.production.yml pull web
-docker-compose -f ./production/docker-compose.production.yml up -d
+ssh root@${PRODUCTION_SERVER} "docker-compose -f ./production/docker-compose.production.yml pull web && docker-compose -f ./production/docker-compose.production.yml up -d
