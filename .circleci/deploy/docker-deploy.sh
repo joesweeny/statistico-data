b0b416cad798b6b83e71e1cdbaa3292f993b1d86
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-15 20:16:36 +0000

    Update copy command

--- .circleci/deploy/docker-deploy.sh
+++ .circleci/deploy/docker-deploy.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-scp docker-compose.production.yml root@${PRODUCTION_SERVER}:~/production/
+scp docker-compose.production.yml root@${PRODUCTION_SERVER}:~/production
 ssh root@${PRODUCTION_SERVER} "docker-compose -f ./production/docker-compose.production.yml pull web && \
 docker-compose -f ./production/docker-compose.production.yml up -d"
