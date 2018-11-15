51eb804a3ed61afd69d6a71ca6914d38c7e19713
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-15 22:35:52 +0000

    Update deply script

--- .circleci/deploy/docker-deploy.sh
+++ .circleci/deploy/docker-deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sed -i 's/\'$RELEASE\'/$(git rev-parse HEAD)/ docker-compose.production.yml'
+sed -i "s/\'$RELEASE\'/$(git rev-parse HEAD)/ docker-compose.production.yml"
 scp docker-compose.production.yml root@${PRODUCTION_SERVER}:~/stats-hub
 ssh root@${PRODUCTION_SERVER} "docker-compose -f ~/stats-hub/docker-compose.production.yml pull web && \
 docker-compose -f ~/stats-hub/docker-compose.production.yml up -d"
