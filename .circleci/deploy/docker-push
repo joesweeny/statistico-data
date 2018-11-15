109accefc599a9db78199463d5c7892c11461a4d
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-15 16:38:15 +0000

    Add docker images script

--- .circleci/deploy/docker-push
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
-docker tag "${DOCKER_IMAGE}:latest" "${DOCKER_HUB_REPOSITORY}/${DOCKER_IMAGE}:latest"
-docker push "${DOCKER_HUB_REPOSITORY}/${DOCKER_IMAGE}:latest"
--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+docker images
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
 docker tag "${DOCKER_IMAGE}:latest" "${DOCKER_HUB_REPOSITORY}/${DOCKER_IMAGE}:latest"
 docker push "${DOCKER_HUB_REPOSITORY}/${DOCKER_IMAGE}:latest"
