4897803a7d309e14b4a546ddfb7b95cbe1b859f2
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-15 22:34:26 +0000

    Update deply script

--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-docker images
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
 docker tag "${DOCKER_IMAGE}:latest" "${DOCKER_HUB_REPOSITORY}/${DOCKER_IMAGE}:$(git rev-parse HEAD)"
 docker push "${DOCKER_HUB_REPOSITORY}/${DOCKER_IMAGE}:$(git rev-parse HEAD)"
