38968b145223f145dfb57d102ebc58dd050f3275
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-16 11:47:40 +0000

    Print docker images in terminal

--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -3,5 +3,6 @@
 set -e
 
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
+docker images
 docker tag "statshub_web" "joesweeny/statshub_web:$(git rev-parse HEAD)"
 docker push "joesweeny/statshub_web:$(git rev-parse HEAD)"
