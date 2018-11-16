6c186d076655cd46db099bcaa538c871746ab8da
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-16 11:53:34 +0000

    Remove bash -s from deploy script

--- .circleci/deploy/docker-deploy.sh
+++ .circleci/deploy/docker-deploy.sh
@@ -5,5 +5,5 @@ set -e
 RELEASE=$(git rev-parse HEAD)
 echo "Release number: $RELEASE"
 scp docker-compose.production.yml root@${PRODUCTION_SERVER}:~
-ssh -t root@${PRODUCTION_SERVER} 'export RELEASE='"'$RELEASE'"'; bash -s && docker-compose -f ./docker-compose.production.yml pull web && \
+ssh -t root@${PRODUCTION_SERVER} 'export RELEASE='"'$RELEASE'"'; docker-compose -f ./docker-compose.production.yml pull web && \
 docker-compose -f ./docker-compose.production.yml up -d'
