09976d0d4b300a2ce781a88de301578793875584
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-16 11:49:44 +0000

    Use correct image

--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -4,5 +4,5 @@ set -e
 
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
 docker images
-docker tag "statshub_web" "joesweeny/statshub_web:$(git rev-parse HEAD)"
-docker push "joesweeny/statshub_web:$(git rev-parse HEAD)"
+docker tag "stats-hub_web" "joesweeny/stats-hub_web:$(git rev-parse HEAD)"
+docker push "joesweeny/stats-hub_web:$(git rev-parse HEAD)"
