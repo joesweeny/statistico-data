f89ed51cc6c55b99c611376787ff463a105f181d
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-20 21:19:39 +0000

    Fix deploy script

--- .circleci/deploy/docker-deploy.sh
+++ .circleci/deploy/docker-deploy.sh
@@ -8,7 +8,7 @@ scp docker-compose.production.yml root@${PRODUCTION_SERVER}:~
 
 ssh -t root@${PRODUCTION_SERVER} \
 'export RELEASE='"'$RELEASE'"'; \
-'export SPORT_MONKS_API_KEY='"'${SPORT_MONKS_API_KEY}'"'; \
+export SPORT_MONKS_API_KEY='"'${SPORT_MONKS_API_KEY}'"'; \
 
 docker-compose -f ./docker-compose.production.yml \
 pull api && docker-compose -f ./docker-compose.production.yml pull console && \
