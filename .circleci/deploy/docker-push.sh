6ee55f56ccc3b9b1f828b00e4413d95ebe9110cb
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-20 14:44:12 +0000

    Update dockerfiles and deploy scripts

--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -4,5 +4,5 @@ set -e
 
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
 docker images
-docker tag "stats-hub_web" "joesweeny/stats-hub_web:$(git rev-parse HEAD)"
-docker push "joesweeny/stats-hub_web:$(git rev-parse HEAD)"
+docker tag "statshub_api" "joesweeny/statshub_api:$(git rev-parse HEAD)"
+docker push "joesweeny/statshub_api:$(git rev-parse HEAD)"
