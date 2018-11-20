630da09a41c9a7b2f6c5615919fbfb784078a34b
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-20 21:30:20 +0000

    Update deploy scripts

--- .circleci/deploy/docker-deploy.sh
+++ .circleci/deploy/docker-deploy.sh
@@ -2,14 +2,8 @@
 
 set -e
 
-RELEASE=$CIRCLE_SHA1
 echo "Release number: $RELEASE"
 scp docker-compose.production.yml root@${PRODUCTION_SERVER}:~
-
-ssh -t root@${PRODUCTION_SERVER} \
-'export RELEASE='"'$RELEASE'"'; \
-export SPORT_MONKS_API_KEY='"'${SPORT_MONKS_API_KEY}'"'; \
-
-docker-compose -f ./docker-compose.production.yml \
-pull api && docker-compose -f ./docker-compose.production.yml pull console && \
+ssh -t root@${PRODUCTION_SERVER} 'docker-compose -f ./docker-compose.production.yml \
+pull api && docker-compose -f ./docker-compose.production.yml && \
 docker-compose -f ./docker-compose.production.yml up -d'
