0cd25857943371f06e72af8a89a611539378d652
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-20 19:04:46 +0000

    Add console application to deploy scripts

--- .circleci/deploy/docker-deploy.sh
+++ .circleci/deploy/docker-deploy.sh
@@ -5,5 +5,6 @@ set -e
 RELEASE=$(git rev-parse HEAD)
 echo "Release number: $RELEASE"
 scp docker-compose.production.yml root@${PRODUCTION_SERVER}:~
-ssh -t root@${PRODUCTION_SERVER} 'export RELEASE='"'$RELEASE'"'; docker-compose -f ./docker-compose.production.yml pull api && \
+ssh -t root@${PRODUCTION_SERVER} 'export RELEASE='"'$RELEASE'"'; docker-compose -f ./docker-compose.production.yml \
+pull api && docker-compose -f ./docker-compose.production.yml && \
 docker-compose -f ./docker-compose.production.yml up -d'
--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -4,5 +4,9 @@ set -e
 
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
 docker images
+
 docker tag "statshub_api" "joesweeny/statshub_api:$(git rev-parse HEAD)"
 docker push "joesweeny/statshub_api:$(git rev-parse HEAD)"
+
+docker tag "statshub_console" "joesweeny/statshub_console:$(git rev-parse HEAD)"
+docker push "joesweeny/statshub_console:$(git rev-parse HEAD)"
