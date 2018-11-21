3e486e4171ec9d7e22936688cf775ec83788ab87
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-11-21 19:38:08 +0000

    New file structure

---
+++ cmd/api/api.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"log"
+	"github.com/gorilla/mux"
+	"os"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", routePath).Methods("GET")
+	router.HandleFunc("/healthcheck", healthCheck).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func routePath(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	fmt.Fprint(w, "Hello " + name + " This actually works!!")
+	w.WriteHeader(http.StatusOK)
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Healthcheck OK")
+	w.WriteHeader(http.StatusOK)
+}
