083c7837b1ed686e1bf0111f3dc80da6b36d65b9
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-04 17:42:39 +0000

    Update postgres test

--- internal/country/repository/postgres_test.go
+++ internal/country/repository/postgres_test.go
@@ -11,53 +11,79 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var c = model.Country{
-	uuid.UUID{},
-	1,
-	"England",
-	"Europe",
-	"ENG",
-	time.Now(),
-	time.Now(),
-}
+var host = os.Getenv("DB_HOST")
+var port = os.Getenv("DB_PORT")
+var user = os.Getenv("DB_USER")
+var pass = os.Getenv("DB_PASSWORD")
+var name = os.Getenv("DB_NAME")
 
 func TestInsert(t *testing.T) {
-	conn := getConnection()
+	conn, cleanUp := getConnection(t)
+	repo := NewPostgresCountryRepository(conn)
 
 	t.Run("insert increases table count", func(t *testing.T) {
-		defer conn.Close()
-		tearDown(conn)
-		repo := NewPostgresCountryRepository(conn)
-		err := repo.Insert(c)
+		t.Helper()
+		sum := 1
+		for i := sum; i < 4; i++ {
+			var id = uuid.Must(uuid.NewV4())
+			var c = model.Country{
+				id,
+				i,
+				"England",
+				"Europe",
+				"ENG",
+				time.Now(),
+				time.Now(),
+			}
 
-		if err != nil {
-			t.Error("Error when inserting record into the database")
+			if err := repo.Insert(c); err != nil {
+				t.Fatalf("Error when inserting record into the database: %s", err.Error())
+			}
+
+			row := conn.QueryRow("select count(*) from country")
+
+			var count int
+
+			if err := row.Scan(&count); err != nil {
+				t.Fatalf("Error when scanning rows returned by the database: %s", err.Error())
+			}
+
+			if sum != count {
+				t.Fatalf("Expected %d, got %d", sum, count)
+			}
+			sum += 1
 		}
 
-		row := conn.QueryRow("select count(*) from country")
-
-		if err != nil {
-			panic(err)
-		}
-
-		var count int
-		err = row.Scan(&count)
-
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Printf("count: %d\n", count)
+		cleanUp()
 	})
+
+	t.Run("insert returns error when ID primary key violates unique constraint", func(t *testing.T) {
+		t.Helper()
+		var c = model.Country{
+			uuid.UUID{},
+			1,
+			"England",
+			"Europe",
+			"ENG",
+			time.Now(),
+			time.Now(),
+		}
+
+		if err := repo.Insert(c); err != nil {
+			t.Fatalf("Test failed, expected nil, got %s", err)
+		}
+
+		if e := repo.Insert(c); e == nil {
+			t.Fatalf("Test failed, expected %s, got nil", e)
+		}
+
+		cleanUp()
+	})
+
+	conn.Close()
 }
 
-func getConnection() *sql.DB {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	pass := os.Getenv("DB_PASSWORD")
-	name := os.Getenv("DB_NAME")
-
+func getConnection(t *testing.T) (*sql.DB, func()) {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, pass, name)
@@ -68,9 +94,10 @@ func getConnection() *sql.DB {
 		panic(err)
 	}
 
-	return db
-}
-
-func tearDown(db *sql.DB) {
-	db.Exec("delete from country")
+	return db, func() {
+		_, err := db.Exec("delete from country")
+		if err != nil {
+			t.Fatalf("Failed to clear database. %s", err.Error())
+		}
+	}
 }
