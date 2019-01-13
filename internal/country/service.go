eceb623bea90a11a833f5465b438b67162c19a69
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-13 19:54:42 +0000

    Move repository logic from service class into handler class

--- internal/country/service.go
+++ internal/country/service.go
@@ -2,9 +2,6 @@ package country
 
 import (
 	"github.com/joesweeny/sportmonks-go-client"
-	"github.com/joesweeny/statshub/internal/model"
-	"github.com/satori/go.uuid"
-	"time"
 )
 
 type Service struct {
@@ -26,10 +23,10 @@ func (s Service) HandleCountries() error {
 			return err
 		}
 
-		for _, country := range res.Data {
-			// Handle error, leaving blank for now - push method into a Go routine
-			s.persistCountry(country)
-		}
+		//for _, _ := range res.Data {
+		//	// Handle error, leaving blank for now - push method into a Go routine
+		//
+		//}
 
 		i++
 	}
@@ -37,36 +34,4 @@ func (s Service) HandleCountries() error {
 	return nil
 }
 
-func (s Service) persistCountry(c sportmonks.Country) error {
-	country, err := s.Repository.GetByExternalId(c.ID)
 
-	if err != nil && (model.Country{}) == country {
-		s.Repository.Insert(create(c))
-		return nil
-	}
-
-	s.Repository.Update(update(c, country))
-
-	return nil
-}
-
-func create(s sportmonks.Country) model.Country {
-	return model.Country{
-		ID: 		uuid.Must(uuid.NewV4(), nil),
-		ExternalID: s.ID,
-		Name: 		s.Name,
-		Continent: 	s.Extra.Continent,
-		ISO: 		s.Extra.ISO,
-		CreatedAt: 	time.Now(),
-		UpdatedAt: 	time.Now(),
-	}
-}
-
-func update(s sportmonks.Country, m model.Country) model.Country {
-	m.ExternalID = s.ID
-	m.Name = s.Name
-	m.Continent = s.Extra.Continent
-	m.ISO = s.Extra.ISO
-
-	return m
-}
