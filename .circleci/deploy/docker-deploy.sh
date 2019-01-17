23f380edc8924e137de32afca1d3399aea46222e
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-17 20:50:01 +0000

    Use same convention for api key env variable

--- .circleci/deploy/docker-deploy.sh
+++ .circleci/deploy/docker-deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 RELEASE=$CIRCLE_SHA1
-API_KEY=${SPORT_MONKS_API_KEY}
+SPORTMONKS_API_KEY=${SPORTMONKS_API_KEY}
 DB_DRIVER=${DB_DRIVER}
 DB_HOST=${DB_HOST}
 DB_NAME=${DB_NAME}
@@ -13,7 +13,7 @@ DB_USER=${DB_USER}
 
 scp docker-compose.production.yml root@${PRODUCTION_SERVER}:~
 
-ssh -t root@${PRODUCTION_SERVER} 'export RELEASE='"'$RELEASE'"' export SPORTMONKS_API_KEY='"'$API_KEY'"' export DB_DRIVER='"'$DB_DRIVER'"' export DB_HOST='"'$DB_HOST'"' export DB_NAME='"'$DB_NAME'"' export DB_NAME='"'$DB_NAME'"' export DB_PASSWORD='"'$DB_PASSWORD'"' export DB_PORT='"'$DB_PORT'"' export DB_USER='"'$DB_USER'"' \
+ssh -t root@${PRODUCTION_SERVER} 'export RELEASE='"'$RELEASE'"' export SPORTMONKS_API_KEY='"'$SPORTMONKS_API_KEY'"' export DB_DRIVER='"'$DB_DRIVER'"' export DB_HOST='"'$DB_HOST'"' export DB_NAME='"'$DB_NAME'"' export DB_NAME='"'$DB_NAME'"' export DB_PASSWORD='"'$DB_PASSWORD'"' export DB_PORT='"'$DB_PORT'"' export DB_USER='"'$DB_USER'"' \
 
 echo $RELEASE && echo $DB_PORT && docker-compose -f ./docker-compose.production.yml pull api && \
 
