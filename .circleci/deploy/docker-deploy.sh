be771b27cffc102981ac52365ac8161d27ad661f
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-17 20:11:01 +0000

    Update deploy script with env variables and update docker-compose production file

--- .circleci/deploy/docker-deploy.sh
+++ .circleci/deploy/docker-deploy.sh
@@ -4,10 +4,22 @@ set -e
 
 RELEASE=$CIRCLE_SHA1
 API_KEY=${SPORT_MONKS_API_KEY}
+DB_DRIVER=${DB_DRIVER}
+DB_HOST=${DB_HOST}
+DB_NAME=${DB_NAME}
+DB_PASSWORD=${DB_PASSWORD}
+DB_PORT=${DB_PORT}
+DB_USER=${DB_USER}
 
 scp docker-compose.production.yml root@${PRODUCTION_SERVER}:~
 
 ssh -t root@${PRODUCTION_SERVER} 'export RELEASE='"'$RELEASE'"' export SPORT_MONKS_API_KEY='"'$API_KEY'"' \
+
+export DB_DRIVER='"'$DB_DRIVER'"' export DB_HOST='"'$DB_HOST'"' export DB_NAME='"'$DB_NAME'"' \
+
+export DB_NAME='"'$DB_NAME'"' export DB_PASSWORD='"'$DB_PASSWORD'"' export DB_PORT='"'$DB_PORT'"' \
+
+export DB_USER='"'$DB_USER'"' \
 
 docker-compose -f ./docker-compose.production.yml pull api && \
 
