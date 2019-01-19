3ac1671ce6bb724763434140d54a2124e32b349a
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-19 20:29:21 +0000

    Add competition struct type

---
+++ internal/model/competition.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+type Competition struct {
+	ID   		 int     `json:"id"`
+	Name  		 string  `json:"name"`
+	CountryID    int     `json:"country_id"`
+	IsCup        bool    `json:"is_cup"`
+	CreatedAt  	 time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
