46c9986ee3dae4a36f774d6294595e62d6802a29
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-19 20:41:56 +0000

    Add postgres repository implementation

---
+++ internal/competition/postgres_repository.go
@@ -0,0 +1,61 @@
+package competition
+
+import (
+	"database/sql"
+	"github.com/joesweeny/statshub/internal/model"
+	_ "github.com/lib/pq"
+	"time"
+)
+
+type PostgresCompetitionRepository struct {
+	Connection *sql.DB
+}
+
+func (p *PostgresCompetitionRepository) Insert(c model.Competition) error {
+	query := `
+	INSERT INTO sportsmonks_competition (id, name, country_id, is_cup, created_at, updated_at)
+	VALUE ($1, $2, $3, $4, $5, $6)`
+
+	_, err := p.Connection.Exec(
+		query,
+		c.ID,
+		c.Name,
+		c.CountryID,
+		c.IsCup,
+		c.CreatedAt.Unix(),
+		c.UpdatedAt.Unix(),
+	)
+
+	return err
+}
+
+func (p *PostgresCompetitionRepository) GetById(id int) (model.Competition, error) {
+	query := `SELECT * FROM sportmonks_country where id = $1`
+	row := p.Connection.QueryRow(query, id)
+
+	return rowToCompetition(row)
+}
+
+func rowToCompetition(r *sql.Row) (model.Competition, error) {
+	var id int
+	var name string
+	var countryId int
+	var isCup bool
+	var created int64
+	var updated int64
+
+	c := model.Competition{}
+
+	if err := r.Scan(&id, &name, &countryId, &isCup, &created, &updated); err != nil {
+		return c, err
+	}
+
+	c.ID = id
+	c.Name = name
+	c.CountryID = countryId
+	c.IsCup = isCup
+	c.CreatedAt = time.Unix(created, 0)
+	c.UpdatedAt = time.Unix(updated, 0)
+
+	return c, nil
+}
