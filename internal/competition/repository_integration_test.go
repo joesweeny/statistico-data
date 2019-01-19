bfd7b5dde949c8a6710f9bd3dbf2f67a0dcaed5e
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-19 20:54:44 +0000

    Add insert repository test

---
+++ internal/competition/repository_integration_test.go
@@ -0,0 +1,71 @@
+package competition
+
+import (
+	"testing"
+	"database/sql"
+	"fmt"
+	"github.com/joesweeny/statshub/internal/config"
+	"github.com/joesweeny/statshub/internal/model"
+	"time"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInsert(t *testing.T) {
+	conn, cleanUp := getConnection(t)
+	repo := PostgresCompetitionRepository{Connection: conn}
+
+	t.Run("increases table count", func (t *testing.T) {
+		t.Helper()
+		defer cleanUp()
+
+		for i := 1; i < 4; i++ {
+			c := newCompetition(i)
+
+			if err := repo.Insert(c); err != nil {
+				t.Errorf("Error when inserting record into the database: %s", err.Error())
+			}
+
+			row := conn.QueryRow("select count(*) from sportmonks_competition")
+
+			var count int
+
+			if err := row.Scan(&count); err != nil {
+				t.Errorf("Error when scanning rows returned by the database: %s", err.Error())
+			}
+
+			assert.Equal(t, i, count)
+		}
+	})
+}
+
+var db = config.GetConfig().Database
+
+func getConnection(t *testing.T) (*sql.DB, func()) {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		db.Host, db.Port, db.User, db.Password, db.Name)
+
+	db, err := sql.Open(db.Driver, psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return db, func() {
+		_, err := db.Exec("delete from sportmonks_competition")
+		if err != nil {
+			t.Fatalf("Failed to clear database. %s", err.Error())
+		}
+	}
+}
+
+func newCompetition(id int) model.Competition {
+	return model.Competition{
+		ID:         id,
+		Name:       "Premier League",
+		CountryID:  462,
+		IsCup:      false,
+		CreatedAt:  time.Unix(1546965200, 0),
+		UpdatedAt:  time.Unix(1546965200, 0),
+	}
+}
