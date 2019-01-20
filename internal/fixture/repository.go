be26239abe9864ecf87505a6aa60dea494d2fda2
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-20 20:21:17 +0000

    Add fixture repository interface

---
+++ internal/fixture/repository.go
@@ -0,0 +1,9 @@
+package fixture
+
+import "github.com/joesweeny/statshub/internal/model"
+
+type Repository interface {
+	Insert(f model.Fixture) error
+	Update(f model.Fixture) error
+	GetById(id int) (model.Fixture, error)
+}
