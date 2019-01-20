b7152710b96f501fd279dd03a3ce808f6db152a5
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-20 21:27:39 +0000

    Make method arguments pointers

--- internal/fixture/repository.go
+++ internal/fixture/repository.go
@@ -3,7 +3,7 @@ package fixture
 import "github.com/joesweeny/statshub/internal/model"
 
 type Repository interface {
-	Insert(f model.Fixture) error
-	Update(f model.Fixture) error
-	GetById(id int) (model.Fixture, error)
+	Insert(f *model.Fixture) error
+	Update(f *model.Fixture) error
+	GetById(id int) (*model.Fixture, error)
 }
