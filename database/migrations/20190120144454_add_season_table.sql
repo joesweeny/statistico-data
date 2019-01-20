0797973b9696c125382008ce730799b456b87615
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-20 14:52:48 +0000

    Add season table migration and season struct

---
+++ database/migrations/20190120144454_add_season_table.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TABLE sportmonks_season (
+  id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  league_id INTEGER NOT NULL,
+  current BOOLEAN NOT NULL,
+  created_at INTEGER NOT NULL,
+  updated_at INTEGER NOT NULL
+);
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE sportmonks_season
