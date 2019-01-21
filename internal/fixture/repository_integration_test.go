8e9b8f04af1289ec4fe466c830cd8bb188177807
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-21 16:47:06 +0000

    Add insert integration tests

---
+++ internal/fixture/repository_integration_test.go
@@ -0,0 +1,92 @@
+package fixture
+
+import (
+	"database/sql"
+	"testing"
+	"fmt"
+	"github.com/joesweeny/statshub/internal/config"
+	"github.com/joesweeny/statshub/internal/model"
+	"gopkg.in/guregu/null.v3"
+	"time"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInsert(t *testing.T) {
+	conn, cleanUp := getConnection(t)
+	repo := PostgresFixtureRepository{Connection: conn}
+
+	t.Run("increases table count", func(t *testing.T) {
+		t.Helper()
+		defer cleanUp()
+
+		for i := 1; i < 4; i++ {
+			c := newFixture(i)
+
+			if err := repo.Insert(c); err != nil {
+				t.Errorf("Error when inserting record into the database: %s", err.Error())
+			}
+
+			row := conn.QueryRow("select count(*) from sportmonks_fixture")
+
+			var count int
+
+			if err := row.Scan(&count); err != nil {
+				t.Errorf("Error when scanning rows returned by the database: %s", err.Error())
+			}
+
+			assert.Equal(t, i, count)
+		}
+	})
+
+	t.Run("returns error when ID primary key violates unique constraint", func(t *testing.T) {
+		t.Helper()
+		defer cleanUp()
+		c := newFixture(50)
+
+		if err := repo.Insert(c); err != nil {
+			t.Errorf("Test failed, expected nil, got %s", err)
+		}
+
+		if e := repo.Insert(c); e == nil {
+			t.Fatalf("Test failed, expected %s, got nil", e)
+		}
+	})
+
+	conn.Close()
+}
+
+var db = config.GetConfig().Database
+
+func getConnection(t *testing.T) (*sql.DB, func()) {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		db.Host, db.Port, db.User, db.Password, db.Name)
+
+	db, err := sql.Open(db.Driver, psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return db, func() {
+		_, err := db.Exec("delete from sportmonks_fixture")
+		if err != nil {
+			t.Fatalf("Failed to clear database. %s", err.Error())
+		}
+	}
+}
+
+func newFixture(id int) *model.Fixture {
+	return &model.Fixture{
+		ID:         id,
+		SeasonID:   14567,
+		RoundID:    null.IntFrom(165789),
+		VenueID:    null.Int{},
+		HomeTeamID: 451,
+		AwayTeamID: 924,
+		RefereeID:  null.Int{},
+		Date:       time.Unix(1548086929, 0),
+		CreatedAt:  time.Unix(1546965200, 0),
+		UpdatedAt:  time.Unix(1546965200, 0),
+	}
+}
