c6fabb9e848e95e3ba2411803b03d4b88fade88c
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-22 21:38:15 +0000

    Add competition service test

---
+++ internal/competition/service_test.go
@@ -0,0 +1,136 @@
+package competition
+
+import (
+	"github.com/stretchr/testify/mock"
+	"github.com/joesweeny/statshub/internal/model"
+	"net/http"
+	"github.com/joesweeny/sportmonks-go-client"
+	"github.com/stretchr/testify/assert"
+	"encoding/json"
+	"io/ioutil"
+	"bytes"
+	"github.com/jonboulle/clockwork"
+	"log"
+	"testing"
+	"errors"
+)
+
+func TestProcess(t *testing.T) {
+	repo := new(mockRepository)
+
+	server := newTestClient(func(req *http.Request) *http.Response {
+		assert.Equal(t, req.URL.String(), "http://example.com/api/v2.0/leagues?api_token=my-key&page=1")
+		b, _ := json.Marshal(leagueResponse())
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBuffer(b)),
+		}
+	})
+
+	client := sportmonks.Client{
+		Client:  server,
+		BaseURL: "http://example.com",
+		ApiKey:  "my-key",
+	}
+
+	service := Service{
+		Repository: repo,
+		Factory:    Factory{clockwork.NewFakeClock()},
+		Client:     &client,
+		Logger:     log.New(ioutil.Discard, "", 0),
+	}
+
+	t.Run("inserts new competition", func (t *testing.T) {
+		repo.On("GetById", 1).Return(&model.Competition{}, errors.New("not Found"))
+		repo.On("Insert", mock.Anything).Return(nil)
+		repo.AssertNotCalled(t, "Update", mock.Anything)
+		service.Process()
+	})
+
+	t.Run("updates existing competition", func (t *testing.T) {
+		c := newCompetition(1)
+		repo.On("GetById", 1).Return(c, nil)
+		repo.On("Update", &c).Return(nil)
+		repo.MethodCalled("Update", &c)
+		repo.AssertNotCalled(t, "Insert", mock.Anything)
+		service.Process()
+	})
+}
+
+
+type mockRepository struct {
+	mock.Mock
+}
+
+func (m mockRepository) Insert(c *model.Competition) error {
+	args := m.Called(c)
+	return args.Error(0)
+}
+
+func (m mockRepository) Update(c *model.Competition) error {
+	args := m.Called(&c)
+	return args.Error(0)
+}
+
+func (m mockRepository) GetById(id int) (*model.Competition, error) {
+	args := m.Called(id)
+	c := args.Get(0).(*model.Competition)
+	return c, args.Error(1)
+}
+
+type roundTripFunc func(req *http.Request) *http.Response
+
+func (r roundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return r(req), nil
+}
+
+func newTestClient(fn roundTripFunc) *http.Client {
+	return &http.Client{
+		Transport: roundTripFunc(fn),
+	}
+}
+
+func leagueResponse() sportmonks.LeaguesResponse {
+	c := clientLeague()
+
+	m := sportmonks.Meta{}
+	m.Pagination.Total = 1
+	m.Pagination.Count = 1
+	m.Pagination.PerPage = 1
+	m.Pagination.CurrentPage = 1
+	m.Pagination.TotalPages = 1
+
+	res := sportmonks.LeaguesResponse{}
+	res.Data = append(res.Data, c)
+	res.Meta = m
+
+	return res
+}
+
+func clientLeague() sportmonks.League {
+	return sportmonks.League{
+		ID:              564,
+		LegacyID:        3491,
+		CountryID:       32,
+		Name:            "Serie A",
+		IsCup:           false,
+		CurrentSeasonID: 23,
+		CurrentRoundID:  98,
+		CurrentStageID:  87,
+		LiveStandings:   true,
+		Coverage: struct {
+			TopscorerGoals   bool `json:"topscorer_goals"`
+			TopscorerAssists bool `json:"topscorer_assists"`
+			TopscorerCards   bool `json:"topscorer_cards"`
+		} {
+			TopscorerGoals: true,
+			TopscorerAssists: false,
+			TopscorerCards: true,
+		},
+		Seasons: struct{
+			Data []sportmonks.Season `json:"data"`
+		} {
+			Data: []sportmonks.Season{},
+		},
+	}
+}
