84f2bf88e71d99da82dbf77f684fbd265bf4d5cb
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-22 17:29:20 +0000

    Add repository integration tests

--- internal/round/repository.go
+++ internal/round/repository.go
@@ -3,7 +3,7 @@ package round
 import "github.com/joesweeny/statshub/internal/model"
 
 type Repository interface {
-	Insert(round *model.Round) error
-	Update(round *model.Round) error
+	Insert(r *model.Round) error
+	Update(r *model.Round) error
 	GetById(id int) (*model.Round, error)
 }
