d967dd7844f2cf3a29bbffd53a9ef378f92c6103
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-22 20:25:25 +0000

    Add factory for competition

---
+++ internal/competition/factory.go
@@ -0,0 +1,30 @@
+package competition
+
+import (
+	"github.com/jonboulle/clockwork"
+	"github.com/joesweeny/sportmonks-go-client"
+	"github.com/joesweeny/statshub/internal/model"
+)
+
+type Factory struct {
+	Clock clockwork.Clock
+}
+
+func (f Factory) createCompetition(s *sportmonks.League) *model.Competition {
+	return &model.Competition{
+		ID:        s.ID,
+		Name:      s.Name,
+		CountryID: s.CountryID,
+		IsCup:     s.IsCup,
+		CreatedAt: f.Clock.Now(),
+		UpdatedAt: f.Clock.Now(),
+	}
+}
+
+func (f Factory) updateCompetition(s *sportmonks.League, m *model.Competition) *model.Competition {
+	m.Name = s.Name
+	m.IsCup = s.IsCup
+	m.UpdatedAt = f.Clock.Now()
+
+	return m
+}
