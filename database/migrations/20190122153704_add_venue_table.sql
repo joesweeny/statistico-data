63c61a6a564c833e0ed1046cb9cffd77a6b007b0
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-22 15:43:47 +0000

    Add venue table migration

---
+++ database/migrations/20190122153704_add_venue_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TABLE sportmonks_venue (
+  id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  surface VARCHAR,
+  address VARCHAR,
+  city VARCHAR,
+  capacity VARCHAR,
+  created_at INTEGER NOT NULL,
+  updated_at INTEGER NOT NULL
+);
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE sportmonks_venue
