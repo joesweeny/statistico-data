193721a018c18d67adccd31782aee73a132ac32a
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-26 10:37:53 +0000

    Add repository interface methods

--- internal/manager/repository.go
+++ internal/manager/repository.go
@@ -1,1 +1,9 @@
 package manager
+
+import "github.com/joesweeny/statshub/internal/model"
+
+type Repository interface {
+	Insert(m *model.Manager) error
+	Update(m *model.Manager) error
+	Id(id int) (*model.Manager, error)
+}
