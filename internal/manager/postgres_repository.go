01edf4f695f827a81b5fed737784fb37dfb206d6
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-26 10:42:36 +0000

    Add implemented insert method

---
+++ internal/manager/postgres_repository.go
@@ -0,0 +1,31 @@
+package manager
+
+import (
+	"database/sql"
+	"github.com/joesweeny/statshub/internal/model"
+)
+
+type PostgresManagerRepository struct {
+	Connection *sql.DB
+}
+
+func (p *PostgresManagerRepository) Insert(m *model.Manager) error {
+	query := `
+	INSERT INTO sportmonks_manager (id, team_id, country_id, first_name, last_name, nationality, image, created_at, 
+	updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+
+	_, err := p.Connection.Exec(
+		query,
+		m.ID,
+		m.TeamID,
+		m.CountryID,
+		m.FirstName,
+		m.LastName,
+		m.Nationality,
+		m.Image,
+		m.CreatedAt.Unix(),
+		m.UpdatedAt.Unix(),
+	)
+
+	return err
+}
