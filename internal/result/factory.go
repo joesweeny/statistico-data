5d6de6828f6d0bc2beaee03f4285892c296ec833
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-02-14 14:08:55 +0000

    Add result factory update method and test

--- internal/result/factory.go
+++ internal/result/factory.go
@@ -34,3 +34,26 @@ func (f Factory) createResult(s *sportmonks.Fixture) *model.Result {
 		UpdatedAt:          f.Clock.Now(),
 	}
 }
+
+func (f Factory) updateResult(s *sportmonks.Fixture, m *model.Result) *model.Result {
+	m.PitchCondition =     s.Pitch
+	m.HomeFormation =      &s.Formations.LocalteamFormation
+	m.AwayFormation =      &s.Formations.VisitorteamFormation
+	m.HomeScore =          &s.Scores.LocalteamScore
+	m.AwayScore =          &s.Scores.VisitorteamScore
+	m.HomePenScore =       s.Scores.LocalteamPenScore
+	m.AwayPenScore =       s.Scores.VisitorteamPenScore
+	m.HalfTimeScore =      s.Scores.HtScore
+	m.FullTimeScore =      s.Scores.FtScore
+	m.ExtraTimeScore =     s.Scores.EtScore
+	m.HomeLeaguePosition = &s.Standings.LocalteamPosition
+	m.AwayLeaguePosition = &s.Standings.VisitorteamPosition
+	m.Minutes =            &s.Time.Minute
+	m.Seconds =            s.Time.Second
+	m.AddedTime =          s.Time.AddedTime
+	m.ExtraTime =          s.Time.ExtraMinute
+	m.InjuryTime =         s.Time.InjuryTime
+	m.UpdatedAt =          f.Clock.Now()
+
+	return m
+}
