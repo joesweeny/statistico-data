5bd6e3b2fa7ac8b468d4c805231cdc03e3cec6d3
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-04-05 18:40:13 +0100

    Add by season ID to fixture repo interface

--- internal/fixture/repository.go
+++ internal/fixture/repository.go
@@ -16,4 +16,8 @@ type Repository interface {
 	// Limit parameter to limit the number of Fixture structs returned
 	// Date constraint returning fixtures from before that date
 	ByTeamId(id int64, limit int32, before time.Time) ([]model.Fixture, error)
+	// Id of the Season
+	// Date constraint returning fixtures from before the given date
+	// Order fixture date 'ASC' or 'DESC'
+	BySeasonId(id int64, limit int32, before time.Time, order string) ([]model.Fixture, error)
 }
