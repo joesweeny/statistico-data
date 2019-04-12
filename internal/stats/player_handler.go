67f29f40d822365bd8c3cdb690338621996becb7
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-04-12 16:36:08 +0100

    Add player stats handler

---
+++ internal/stats/player_handler.go
@@ -0,0 +1,18 @@
+package stats
+
+import (
+	"github.com/statistico/statistico-data/internal/model"
+	"github.com/statistico/statistico-data/internal/proto"
+	pbPlayerStats "github.com/statistico/statistico-data/internal/proto/stats"
+)
+
+func HandlePlayerStats(p *[]model.PlayerStats) ([]*pbPlayerStats.PlayerStats) {
+	var stats []*pbPlayerStats.PlayerStats
+
+	for _, player := range *p {
+		s := proto.PlayerStatsToProto(&player)
+		stats = append(stats, s)
+	}
+
+	return stats
+}
