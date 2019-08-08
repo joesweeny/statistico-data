d5be480200c67b685546c4c22f36eb4b3e472c73
Joe Sweeny <joesweeny84@hotmail.com>
2019-08-08 15:50:14 +0100

    Add migration to remove seconds column from result table

--- internal/result/postgres_repository.go
+++ internal/result/postgres_repository.go
@@ -18,8 +18,8 @@ func (p *PostgresResultRepository) Insert(r *model.Result) error {
 	query := `
 	INSERT INTO sportmonks_result (fixture_id, pitch_condition, home_formation, away_formation, home_score, away_score,
 	home_pen_score, away_pen_score, half_time_score, full_time_score, extra_time_score, home_league_position,
-	away_league_position, minutes, seconds, added_time, extra_time, injury_time, created_at, updated_at) VALUES ($1, $2, 
-	$3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20)`
+	away_league_position, minutes, added_time, extra_time, injury_time, created_at, updated_at) VALUES ($1, $2, 
+	$3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)`
 
 	_, err := p.Connection.Exec(
 		query,
@@ -57,8 +57,8 @@ func (p *PostgresResultRepository) Update(r *model.Result) error {
 	query := `
 	UPDATE sportmonks_result SET pitch_condition = $2, home_formation = $3, away_formation = $4, home_score = $5, 
     away_score = $6, home_pen_score = $7, away_pen_score = $8, half_time_score = $9, full_time_score = $10, 
-	extra_time_score = $11, home_league_position = $12, away_league_position = $13, minutes = $14, seconds = $15, 
-	added_time = $16, extra_time = $17, injury_time = $18, updated_at = $19 WHERE fixture_id = $1`
+	extra_time_score = $11, home_league_position = $12, away_league_position = $13, minutes = $14, 
+	added_time = $15, extra_time = $16, injury_time = $17, updated_at = $18 WHERE fixture_id = $1`
 
 	_, err = p.Connection.Exec(
 		query,
