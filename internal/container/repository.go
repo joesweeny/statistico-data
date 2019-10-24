d89942d33c0096b62beb20e0ffeb5f0dc3663f3d
Joe Sweeny <joesweeny84@hotmail.com>
2019-10-24 17:27:16 +0100

    Add venue repository to container

--- internal/container/repository.go
+++ internal/container/repository.go
@@ -7,3 +7,7 @@ import (
 func (c Container) CountryRepository() *postgres.CountryRepository {
 	return postgres.NewCountryRepository(c.Database, c.Clock)
 }
+
+func (c Container) VenueRepository() *postgres.VenueRepository {
+	return postgres.NewVenueRepository(c.Database, c.Clock)
+}
