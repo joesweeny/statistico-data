70d5521d43ea61fb328b854de16465d2d9f6946e
Joe Sweeny <joesweeny84@hotmail.com>
2019-10-29 14:03:02 +0000

    Add requester team tests

---
+++ internal/app/sportmonks/team_test.go
@@ -0,0 +1,97 @@
+package sportmonks_test
+
+import (
+	"bytes"
+	"github.com/sirupsen/logrus/hooks/test"
+	"github.com/statistico/statistico-data/internal/app/mock"
+	"github.com/statistico/statistico-data/internal/app/sportmonks"
+	spClient "github.com/statistico/statistico-sportmonks-go-client"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func TestTeamsBySeasonIDs(t *testing.T) {
+	t.Run("returns channel containing team struct", func(t *testing.T) {
+		server := mock.HttpClient(func(req *http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       ioutil.NopCloser(bytes.NewBufferString(teamsResponse)),
+			}, nil
+		})
+
+		client := spClient.HTTPClient{
+			HTTPClient: server,
+			BaseURL:    "http://example.com",
+			Key:        "my-key",
+		}
+
+		logger, _ := test.NewNullLogger()
+
+		requester := sportmonks.NewTeamRequester(&client, logger)
+
+		ch := requester.TeamsBySeasonIDs([]int64{int64(435), int64(33), int64(2)})
+
+		x := <- ch
+		y := <- ch
+		z := <- ch
+
+		a := assert.New(t)
+
+		a.Equal(int64(1), x.ID)
+		a.Equal("West Ham United", x.Name)
+		a.Equal("WHU", *x.ShortCode)
+		a.Equal(int64(462), x.CountryID)
+		a.Equal(int64(214), x.VenueID)
+		a.Equal(false, x.NationalTeam)
+		a.Equal(1895, *x.Founded)
+		a.Equal("https://cdn.sportmonks.com/images/soccer/teams/1/1.png", *x.Logo)
+
+		a.Equal(int64(1), y.ID)
+		a.Equal("West Ham United", y.Name)
+		a.Equal("WHU", *y.ShortCode)
+		a.Equal(int64(462), y.CountryID)
+		a.Equal(int64(214), y.VenueID)
+		a.Equal(false, y.NationalTeam)
+		a.Equal(1895, *y.Founded)
+		a.Equal("https://cdn.sportmonks.com/images/soccer/teams/1/1.png", *y.Logo)
+
+		a.Equal(int64(1), z.ID)
+		a.Equal("West Ham United", z.Name)
+		a.Equal("WHU", *z.ShortCode)
+		a.Equal(int64(462), z.CountryID)
+		a.Equal(int64(214), z.VenueID)
+		a.Equal(false, z.NationalTeam)
+		a.Equal(1895, *z.Founded)
+		a.Equal("https://cdn.sportmonks.com/images/soccer/teams/1/1.png", *z.Logo)
+	})
+}
+
+var teamsResponse = `{
+	"data": [
+		{
+			"id": 1,
+			"legacy_id": 377,
+			"name": "West Ham United",
+			"short_code": "WHU",
+			"twitter": "@WestHamUtd",
+			"country_id": 462,
+			"national_team": false,
+			"founded": 1895,
+			"logo_path": "https:\/\/cdn.sportmonks.com\/images\/soccer\/teams\/1\/1.png",
+			"venue_id": 214,
+			"current_season_id": 16036
+		}
+	],
+	"meta": {
+		"pagination": {
+			"total": 1,
+			"count": 1,
+			"per_page": 100,
+			"current_page": 1,
+			"total_pages": 1,
+			"links": {}
+		}
+	}
+}`
