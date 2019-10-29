aed51157e606d641e38df92f6215b3eeee219b9e
Joe Sweeny <joesweeny84@hotmail.com>
2019-10-29 19:56:05 +0000

    Add player struct and repository interface

---
+++ internal/app/player.go
@@ -0,0 +1,26 @@
+package app
+
+import (
+	"time"
+)
+
+// Player domain entity.
+type Player struct {
+	ID          int64       `json:"id"`
+	CountryId   int64       `json:"country_id"`
+	FirstName   string    `json:"first_name"`
+	LastName    string    `json:"last_name"`
+	BirthPlace  *string   `json:"birth_place"`
+	DateOfBirth *string   `json:"date_of_birth"`
+	PositionID  int       `json:"position_id"`
+	Image       *string   `json:"image_path"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+// PlayerRepository provides an interface to persist Player domain struct objects to a storage engine.
+type PlayerRepository interface {
+	Insert(m *Player) error
+	Update(m *Player) error
+	ByID(id int64) (*Player, error)
+}
