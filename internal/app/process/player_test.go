e8708e2e1f0abe81a41c32572aac4e5ab198b5f9
Joe Sweeny <joesweeny84@hotmail.com>
2019-10-30 11:05:44 +0000

    Add additional player processor test

--- internal/app/process/player_test.go
+++ internal/app/process/player_test.go
@@ -54,6 +54,50 @@ func TestPlayerProcessor_Process(t *testing.T) {
 		squadRepo.AssertExpectations(t)
 		assert.Nil(t, hook.LastEntry())
 	})
+
+	t.Run("does not insert player if already exists", func(t *testing.T) {
+		t.Helper()
+
+		playerRepo := new(mock.PlayerRepository)
+		squadRepo := new(mock.SquadRepository)
+		requester := new(mock.PlayerRequester)
+		logger, hook := test.NewNullLogger()
+
+		processor := process.NewPlayerProcessor(playerRepo, squadRepo, requester, logger)
+
+		done := make(chan bool)
+
+		def := newPlayer(1)
+		mid := newPlayer(2)
+		str := newPlayer(3)
+
+		squad := newSquad()
+
+		squadRepo.On("All").Return(squad, nil)
+
+		playerRepo.On("ByID", int64(1)).Return(&app.Player{}, errors.New("not found"))
+		playerRepo.On("ByID", int64(2)).Return(&app.Player{}, nil)
+		playerRepo.On("ByID", int64(3)).Return(&app.Player{}, errors.New("not found"))
+
+		requester.On("PlayerByID", int64(1)).Return(def)
+		requester.On("PlayerByID", int64(3)).Return(str)
+
+		requester.AssertNotCalled(t, "PlayerByID", int64(2))
+
+		playerRepo.On("Insert", def).Return(nil)
+		playerRepo.On("Insert", str).Return(nil)
+
+		playerRepo.AssertNotCalled(t, "Insert", mid)
+
+		processor.Process("player", "", done)
+
+		<-done
+
+		requester.AssertExpectations(t)
+		playerRepo.AssertExpectations(t)
+		squadRepo.AssertExpectations(t)
+		assert.Nil(t, hook.LastEntry())
+	})
 }
 
 func newPlayer(id int64) *app.Player {
