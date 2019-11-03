6476b981cbf2b540ba70feb6ae9343a44f2ba4da
Joe Sweeny <joesweeny84@hotmail.com>
2019-11-03 11:03:50 +0000

    Add fixture entity and repository interface

---
+++ internal/app/fixture.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+	"time"
+)
+
+// Fixture domain entity.
+type Fixture struct {
+	ID         int64      `json:"id"`
+	SeasonID   int64       `json:"season_id"`
+	RoundID    *int64      `json:"round_id"`
+	VenueID    *int64      `json:"venue_id"`
+	HomeTeamID int64       `json:"home_team_id"`
+	AwayTeamID int64       `json:"away_team_id"`
+	RefereeID  *int64      `json:"referee_id"`
+	Date       time.Time `json:"date"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+// FixtureRepository provides an interface to persist Fixture domain struct objects to a storage engine.
+type FixtureRepository interface {
+	Insert(f *Fixture) error
+	Update(f *Fixture) error
+	ById(id uint64) (*Fixture, error)
+	IDs() ([]uint64, error)
+	IDsBetween(from, to time.Time) ([]uint64, error)
+	Between(from, to time.Time) ([]Fixture, error)
+	ByTeamID(id int64, limit int32, before time.Time) ([]Fixture, error)
+	BySeasonID(id int64, before time.Time) ([]Fixture, error)
+	ByHomeAndAwayTeam(homeTeamId, awayTeamId int64, limit uint32, before time.Time) ([]Fixture, error)
+}
