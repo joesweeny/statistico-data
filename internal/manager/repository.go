fb7b2371a7442031e0a6c0181b217eb7514df853
Joe Sweeny <joesweeny84@hotmail.com>
2019-11-03 22:06:09 +0000

    Delete manager code

--- internal/manager/repository.go
+++
@@ -1,9 +0,0 @@
-package manager
-
-import "github.com/statistico/statistico-data/internal/model"
-
-type Repository interface {
-	Insert(m *model.Manager) error
-	Update(m *model.Manager) error
-	Id(id int) (*model.Manager, error)
-}
