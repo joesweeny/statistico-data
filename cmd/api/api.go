dc2353e795c5ff215dfe6d4f6196e149b6d22292
Joe Sweeny <joesweeny84@hotmail.com>
2019-11-12 19:44:10 +0000

    Add api service to docker-compose build file

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,7 +5,6 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
@@ -14,12 +13,11 @@ func main() {
 	router.HandleFunc("/", routePath).Methods("GET")
 	router.HandleFunc("/healthcheck", healthCheck).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":80", router))
 }
 
 func routePath(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	fmt.Fprint(w, "Hello "+name+" This actually works!!")
+	fmt.Fprint(w, "Hello This actually works!!")
 	w.WriteHeader(http.StatusOK)
 }
 
