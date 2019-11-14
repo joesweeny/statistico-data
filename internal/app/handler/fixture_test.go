ae6f8ca69bc680f4b6618e945cc9dde713a5d356
Joe Sweeny <joesweeny84@hotmail.com>
2019-11-14 12:32:36 +0000

    Update fixture spec for rest and grpc

--- internal/app/handler/fixture_test.go
+++ internal/app/handler/fixture_test.go
@@ -1,9 +1,7 @@
 package handler
 
 import (
-	"errors"
 	"github.com/sirupsen/logrus/hooks/test"
-	"github.com/statistico/statistico-data/internal/app"
 	m "github.com/statistico/statistico-data/internal/app/mock"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -18,9 +16,7 @@ func TestHandleFixture(t *testing.T) {
 	venueRepo := new(m.VenueRepository)
 	handler := FixtureHandler{
 		TeamRepo:        teamRepo,
-		CompetitionRepo: compRepo,
 		RoundRepo:       roundRepo,
-		SeasonRepo:      seasonRepo,
 		VenueRepo:       venueRepo,
 		Logger:          logger,
 	}
@@ -34,8 +30,6 @@ func TestHandleFixture(t *testing.T) {
 		fixture.VenueID = &ven
 		fixture.RefereeID = &ref
 
-		seasonRepo.On("ByID", uint64(14567)).Return(newSeason(), nil)
-		compRepo.On("ByID", uint64(45)).Return(newCompetition(), nil)
 		teamRepo.On("ByID", uint64(451)).Return(newTeam(451, "West Ham"), nil)
 		teamRepo.On("ByID", uint64(924)).Return(newTeam(924, "Chelsea"), nil)
 		venueRepo.On("GetById", uint64(87)).Return(newVenue(), nil)
@@ -50,20 +44,13 @@ func TestHandleFixture(t *testing.T) {
 		a := assert.New(t)
 
 		a.Equal(int64(99), proto.Id)
-		a.Equal(int64(4), proto.Competition.GetId())
-		a.Equal("Premier League", proto.Competition.GetName())
-		a.False(proto.Competition.IsCup.GetValue())
-		a.Equal(int64(14567), proto.Season.GetId())
-		a.Equal("2018-2019", proto.Season.GetName())
-		a.True(proto.Season.IsCurrent.GetValue())
 		a.Equal(int64(451), proto.HomeTeam.GetId())
 		a.Equal("West Ham", proto.HomeTeam.GetName())
 		a.Equal(int64(924), proto.AwayTeam.GetId())
 		a.Equal("Chelsea", proto.AwayTeam.GetName())
 		a.Equal(int64(87), proto.Venue.GetId().GetValue())
 		a.Equal("London Stadium", proto.Venue.GetName().GetValue())
-		a.Equal(int64(5), proto.RefereeId.GetValue())
-		a.Equal(int64(1548086929), proto.GetDateTime())
+		a.Equal(int64(1548086929), proto.GetDateTime().GetUtc())
 		a.Equal(int64(165789), proto.Round.GetId())
 		a.Equal("18", proto.Round.GetName())
 		a.Equal(int64(14567), proto.Round.GetSeasonId())
@@ -89,56 +76,13 @@ func TestHandleFixture(t *testing.T) {
 		a := assert.New(t)
 
 		a.Equal(int64(99), proto.Id)
-		a.Equal(int64(4), proto.Competition.GetId())
-		a.Equal("Premier League", proto.Competition.GetName())
-		a.Equal(int64(14567), proto.Season.GetId())
-		a.Equal("2018-2019", proto.Season.GetName())
 		a.Equal(int64(451), proto.HomeTeam.GetId())
 		a.Equal("West Ham", proto.HomeTeam.GetName())
 		a.Equal(int64(924), proto.AwayTeam.GetId())
 		a.Equal("Chelsea", proto.AwayTeam.GetName())
 		a.Nil(proto.Venue.GetId())
 		a.Nil(proto.Venue.GetName())
-		a.Nil(proto.RefereeId)
-		a.Equal(int64(1548086929), proto.GetDateTime())
+		a.Equal(int64(1548086929), proto.GetDateTime().GetUtc())
 		a.Nil(proto.Round)
-	})
-
-	t.Run("error is returned if season not found", func(t *testing.T) {
-		logger, _ := test.NewNullLogger()
-		teamRepo := new(m.TeamRepository)
-		compRepo := new(m.CompetitionRepository)
-		roundRepo := new(m.RoundRepository)
-		seasonRepo := new(m.SeasonRepository)
-		venueRepo := new(m.VenueRepository)
-		handler := FixtureHandler{
-			TeamRepo:        teamRepo,
-			CompetitionRepo: compRepo,
-			RoundRepo:       roundRepo,
-			SeasonRepo:      seasonRepo,
-			VenueRepo:       venueRepo,
-			Logger:          logger,
-		}
-
-		ven := uint64(87)
-		fixture := newFixture(99)
-		fixture.VenueID = &ven
-
-		seasonRepo.On("ByID", uint64(14567)).Return(&app.Season{}, errors.New("not found"))
-		compRepo.AssertNotCalled(t, "GetById", 45)
-		teamRepo.AssertNotCalled(t, "ByID", int64(451))
-		teamRepo.AssertNotCalled(t, "ByID", int64(924))
-		venueRepo.AssertNotCalled(t, "GetById", 87)
-		roundRepo.AssertNotCalled(t, "ByID", int64(165789))
-
-		proto, err := handler.HandleFixture(fixture)
-
-		if proto != nil {
-			t.Fatalf("Test failed, expected nil, got %+v", proto)
-		}
-
-		if err == nil {
-			t.Fatalf("Test failed, expected %s, got nil", errors.New("not found"))
-		}
 	})
 }
