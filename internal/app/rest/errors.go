933ded810b41769db8b4b7646779217f47268ecf
Joe Sweeny <joesweeny84@hotmail.com>
2019-11-14 20:06:32 +0000

    Add errors import

--- internal/app/rest/errors.go
+++ internal/app/rest/errors.go
@@ -1,4 +1,5 @@
 package rest
 
-var errTimeParse = errors.New("date provided in request is not a valid RFC3339 formatted date")
+import "errors"
 
+var errTimeParse = errors.New("date provided in request is not a valid RFC3339 formatted date")
