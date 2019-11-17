0af6b6a1099b98975de3773202aefaddd81fb71b
Joe Sweeny <joesweeny84@hotmail.com>
2019-11-17 17:33:27 +0000

    Update API spec and render documentation

--- internal/app/rest/handlers.go
+++ internal/app/rest/handlers.go
@@ -3,15 +3,33 @@ package rest
 import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func RoutePath(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.WriteHeader(http.StatusOK)
-	_, _ = fmt.Fprint(w, "Welcome to the Statistico Data API")
+	http.ServeFile(w, r, "./opt/api/index.html")
 }
 
 func HealthCheck(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.WriteHeader(http.StatusOK)
 	_, _ = fmt.Fprint(w, "Healthcheck OK")
+}
+
+func RenderApiDocs(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
+
+	json, err := os.Open("./opt/api/openapi.json")
+
+	defer json.Close()
+
+	if err != nil {
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
+
+	contents, _ := ioutil.ReadAll(json)
+	_, _ = w.Write(contents)
 }
