e680de9c06acbaf9454946544c62afe12efd5568
Joe Sweeny <joesweeny84@hotmail.com>
2019-11-20 16:46:50 +0000

    Add fixture team xg repository interface

--- internal/app/team_xg.go
+++ internal/app/team_xg.go
@@ -10,3 +10,11 @@ type FixtureTeamXG struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+// FixtureTeamXGRepository provides an interface to persist FixtureTeamXG domain struct objects to a storage engine.
+type FixtureTeamXGRepository interface {
+	Insert(f FixtureTeamXG) error
+	Update(f FixtureTeamXG) error
+	ByID(id uint64) (*FixtureTeamXG, error)
+	ByFixtureID(id uint64) (*FixtureTeamXG, error)
+}
