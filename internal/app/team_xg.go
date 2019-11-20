f51abe0188ab0c4ff683d5636398cdf0d7a855e5
Joe Sweeny <joesweeny84@hotmail.com>
2019-11-20 13:42:40 +0000

    Add ID field to struct

--- internal/app/team_xg.go
+++ internal/app/team_xg.go
@@ -3,6 +3,7 @@ package app
 import "time"
 
 type FixtureTeamXG struct {
+	ID        uint64    `json:"id"`
 	FixtureID uint64    `json:"fixture_id"`
 	Home      float32   `json:"home"`
 	Away      float32   `json:"away"`
