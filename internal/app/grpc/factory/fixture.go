c75b53cef15f21d8a918f232fb7e7aefa5858aa1
Joe Sweeny <joesweeny84@hotmail.com>
2019-11-25 18:12:32 +0000

    Add season hydration to fixture factory

--- internal/app/grpc/factory/fixture.go
+++ internal/app/grpc/factory/fixture.go
@@ -9,6 +9,7 @@ import (
 
 type FixtureFactory struct {
 	roundRepo app.RoundRepository
+	seasonRepo app.SeasonRepository
 	teamRepo  app.TeamRepository
 	venueRepo app.VenueRepository
 	logger    *logrus.Logger
@@ -27,10 +28,17 @@ func (b FixtureFactory) BuildFixture(f *app.Fixture) (*proto.Fixture, error) {
 		return nil, b.returnLoggedError(f.ID, err)
 	}
 
+	season, err := b.seasonRepo.ByID(f.SeasonID)
+
+	if err != nil {
+		return nil, b.returnLoggedError(f.ID, err)
+	}
+
 	p := proto.Fixture{
 		Id:       int64(f.ID),
 		HomeTeam: teamToProto(home),
 		AwayTeam: teamToProto(away),
+		Season:   seasonToProto(season),
 		DateTime: &proto.Date{
 			Utc: f.Date.Unix(),
 			Rfc: f.Date.Format(time.RFC3339),
@@ -61,6 +69,12 @@ func (b FixtureFactory) returnLoggedError(id uint64, err error) error {
 	return err
 }
 
-func NewFixtureFactory(r app.RoundRepository, t app.TeamRepository, v app.VenueRepository, log *logrus.Logger) *FixtureFactory {
-	return &FixtureFactory{roundRepo: r, teamRepo: t, venueRepo: v, logger: log}
+func NewFixtureFactory(
+	r app.RoundRepository,
+	s app.SeasonRepository,
+	t app.TeamRepository,
+	v app.VenueRepository, 
+	log *logrus.Logger,
+) *FixtureFactory {
+	return &FixtureFactory{roundRepo: r, seasonRepo: s, teamRepo: t, venueRepo: v, logger: log}
 }
