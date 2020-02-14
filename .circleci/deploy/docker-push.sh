a3171c66376e478baad935a20f9a03c969d453c6
Joe Sweeny <joesweeny84@hotmail.com>
2020-02-14 16:30:27 +0000

    Add aws login script to push script

--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION | bash
+
 docker tag "statisticodata_rest" "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/rest:$CIRCLE_SHA1"
 docker push "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/rest:$CIRCLE_SHA1"
