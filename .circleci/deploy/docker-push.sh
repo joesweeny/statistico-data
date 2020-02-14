add02d52eba7584e3cd387c5d8a5ce91402a4291
Joe Sweeny <joesweeny84@hotmail.com>
2020-02-14 14:04:15 +0000

    Add temporary deploy scripts

--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -2,20 +2,5 @@
 
 set -e
 
-docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
-docker images
-
-docker tag "statisticodata_console" "joesweeny/statisticodata_console:$CIRCLE_SHA1"
-docker push "joesweeny/statisticodata_console:$CIRCLE_SHA1"
-
-docker tag "statisticodata_migrate" "joesweeny/statisticodata_migrate:$CIRCLE_SHA1"
-docker push "joesweeny/statisticodata_migrate:$CIRCLE_SHA1"
-
-docker tag "statisticodata_cron" "joesweeny/statisticodata_cron:$CIRCLE_SHA1"
-docker push "joesweeny/statisticodata_cron:$CIRCLE_SHA1"
-
-docker tag "statisticodata_grpc" "joesweeny/statisticodata_grpc:$CIRCLE_SHA1"
-docker push "joesweeny/statisticodata_grpc:$CIRCLE_SHA1"
-
-docker tag "statisticodata_rest" "joesweeny/statisticodata_rest:$CIRCLE_SHA1"
-docker push "joesweeny/statisticodata_rest:$CIRCLE_SHA1"
+docker tag "statisticodata_rest" "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/rest:$CIRCLE_SHA1"
+docker push "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/rest:$CIRCLE_SHA1"
