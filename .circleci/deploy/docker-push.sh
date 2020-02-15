9ec6b78a7a03303f9f1694b4e532537a4a8794d0
Joe Sweeny <joesweeny84@hotmail.com>
2020-02-15 17:11:50 +0000

    Add docker push scripts

--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -4,5 +4,14 @@ set -e
 
 aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION | bash
 
+docker tag "statisticodata_console" "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/console:$CIRCLE_SHA1"
+docker push "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/console:$CIRCLE_SHA1"
+
+docker tag "statisticodata_cron" "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/cron:$CIRCLE_SHA1"
+docker push "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/cron:$CIRCLE_SHA1"
+
+docker tag "statisticodata_grpc" "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/grpc:$CIRCLE_SHA1"
+docker push "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/grpc:$CIRCLE_SHA1"
+
 docker tag "statisticodata_rest" "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/rest:$CIRCLE_SHA1"
 docker push "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/rest:$CIRCLE_SHA1"
