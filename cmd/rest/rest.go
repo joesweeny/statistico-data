9bad99427b9d77f22a391b4426f2dba6c0d1da8b
Joe Sweeny <joesweeny84@hotmail.com>
2020-02-15 16:15:20 +0000

    Add temporary main function

--- cmd/rest/rest.go
+++ cmd/rest/rest.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"github.com/julienschmidt/httprouter"
-	"github.com/statistico/statistico-data/internal/app/rest"
-	"github.com/statistico/statistico-data/internal/bootstrap"
-	"log"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	container := bootstrap.BuildContainer(bootstrap.BuildConfig())
-
-	router := httprouter.New()
-	router.GET("/", rest.RoutePath)
-	router.GET("/healthcheck", rest.HealthCheck)
-	router.GET("/openapi.json", rest.RenderApiDocs)
-	router.GET("/season/:id/fixtures", container.RestFixtureHandler().SeasonFixtures)
-
-	log.Fatal(http.ListenAndServe(":80", router))
+	fmt.Println("Hello World")
 }
