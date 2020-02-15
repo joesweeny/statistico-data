d84efed9c8bf0124103e7c75c8a5997a34516f8d
Joe Sweeny <joesweeny84@hotmail.com>
2020-02-15 20:06:38 +0000

    Remove cron and console services

--- .circleci/deploy/docker-load.sh
+++ .circleci/deploy/docker-load.sh
@@ -2,7 +2,5 @@
 
 set -e
 
-docker load -i /tmp/workspace/docker-cache/statisticodata_console.tar
-docker load -i /tmp/workspace/docker-cache/statisticodata_cron.tar
 docker load -i /tmp/workspace/docker-cache/statisticodata_grpc.tar
 docker load -i /tmp/workspace/docker-cache/statisticodata_rest.tar
--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -4,12 +4,6 @@ set -e
 
 aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION | bash
 
-docker tag "statisticodata_console" "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/console:$CIRCLE_SHA1"
-docker push "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/console:$CIRCLE_SHA1"
-
-docker tag "statisticodata_cron" "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/cron:$CIRCLE_SHA1"
-docker push "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/cron:$CIRCLE_SHA1"
-
 docker tag "statisticodata_grpc" "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/grpc:$CIRCLE_SHA1"
 docker push "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data/grpc:$CIRCLE_SHA1"
 
