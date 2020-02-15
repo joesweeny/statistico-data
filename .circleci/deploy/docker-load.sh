e00c284ab904221c602d41dd85905314b0ebeea9
Joe Sweeny <joesweeny84@hotmail.com>
2020-02-15 21:05:53 +0000

    Add migrate service

--- .circleci/deploy/docker-load.sh
+++ .circleci/deploy/docker-load.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker load -i /tmp/workspace/docker-cache/statisticodata_grpc.tar
+docker load -i /tmp/workspace/docker-cache/statisticodata_migrate.tar
 docker load -i /tmp/workspace/docker-cache/statisticodata_rest.tar
--- .circleci/deploy/docker-save.sh
+++ .circleci/deploy/docker-save.sh
@@ -4,5 +4,5 @@ set -e
 
 mkdir -p /tmp/workspace/docker-cache
 
-docker save -o /tmp/workspace/docker-cache/statisticodata_grpc.tar statisticodata_grpc:latest
+docker save -o /tmp/workspace/docker-cache/statisticodata_migrate.tar statisticodata_migrate:latest
 docker save -o /tmp/workspace/docker-cache/statisticodata_rest.tar statisticodata_rest:latest
