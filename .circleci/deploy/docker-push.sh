317e0e17658db9a47a8158c2e003a328fa75b598
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-06-03 15:40:00 +0100

    Add temporary deployment scripts

--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -4,5 +4,5 @@ set -e
 
 aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION | bash
 
-docker tag "statisticodata_console" "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data:$CIRCLE_SHA1"
-docker push "216629550457.dkr.ecr.eu-west-2.amazonaws.com/statistico-data:$CIRCLE_SHA1"
+docker tag "statisticodata_console" $AWS_ECR_ACCOUNT_URL:$CIRCLE_SHA1"
+docker push $AWS_ECR_ACCOUNT_URL:$CIRCLE_SHA1"
