622db4e98612191826c2295dbfad7cdace8cebd9
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-06-18 18:44:09 +0100

    Update performance service method name

--- internal/app/grpc/proto/performance.pb.go
+++ internal/app/grpc/proto/performance.pb.go
@@ -203,15 +203,15 @@ var file_internal_app_grpc_proto_performance_proto_rawDesc = []byte{
 	0x65, 0x6e, 0x75, 0x65, 0x22, 0x35, 0x0a, 0x10, 0x54, 0x65, 0x61, 0x6d, 0x53, 0x74, 0x61, 0x74,
 	0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x21, 0x0a, 0x05, 0x74, 0x65, 0x61, 0x6d,
 	0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x0b, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2e,
-	0x54, 0x65, 0x61, 0x6d, 0x52, 0x05, 0x74, 0x65, 0x61, 0x6d, 0x73, 0x32, 0x60, 0x0a, 0x12, 0x50,
+	0x54, 0x65, 0x61, 0x6d, 0x52, 0x05, 0x74, 0x65, 0x61, 0x6d, 0x73, 0x32, 0x5f, 0x0a, 0x12, 0x50,
 	0x65, 0x72, 0x66, 0x6f, 0x72, 0x6d, 0x61, 0x6e, 0x63, 0x65, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63,
-	0x65, 0x12, 0x4a, 0x0a, 0x15, 0x47, 0x65, 0x74, 0x54, 0x65, 0x61, 0x6d, 0x73, 0x4d, 0x61, 0x74,
-	0x63, 0x68, 0x69, 0x6e, 0x67, 0x53, 0x74, 0x61, 0x74, 0x73, 0x12, 0x16, 0x2e, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x2e, 0x54, 0x65, 0x61, 0x6d, 0x53, 0x74, 0x61, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65,
-	0x73, 0x74, 0x1a, 0x17, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2e, 0x54, 0x65, 0x61, 0x6d, 0x53,
-	0x74, 0x61, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x19, 0x5a,
-	0x17, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x61, 0x70, 0x70, 0x2f, 0x67, 0x72,
-	0x70, 0x63, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x65, 0x12, 0x49, 0x0a, 0x14, 0x47, 0x65, 0x74, 0x54, 0x65, 0x61, 0x6d, 0x73, 0x4d, 0x61, 0x74,
+	0x63, 0x68, 0x69, 0x6e, 0x67, 0x53, 0x74, 0x61, 0x74, 0x12, 0x16, 0x2e, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x2e, 0x54, 0x65, 0x61, 0x6d, 0x53, 0x74, 0x61, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
+	0x74, 0x1a, 0x17, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2e, 0x54, 0x65, 0x61, 0x6d, 0x53, 0x74,
+	0x61, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x19, 0x5a, 0x17,
+	0x69, 0x6e, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x61, 0x70, 0x70, 0x2f, 0x67, 0x72, 0x70,
+	0x63, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -234,8 +234,8 @@ var file_internal_app_grpc_proto_performance_proto_goTypes = []interface{}{
 }
 var file_internal_app_grpc_proto_performance_proto_depIdxs = []int32{
 	2, // 0: proto.TeamStatResponse.teams:type_name -> proto.Team
-	0, // 1: proto.PerformanceService.GetTeamsMatchingStats:input_type -> proto.TeamStatRequest
-	1, // 2: proto.PerformanceService.GetTeamsMatchingStats:output_type -> proto.TeamStatResponse
+	0, // 1: proto.PerformanceService.GetTeamsMatchingStat:input_type -> proto.TeamStatRequest
+	1, // 2: proto.PerformanceService.GetTeamsMatchingStat:output_type -> proto.TeamStatResponse
 	2, // [2:3] is the sub-list for method output_type
 	1, // [1:2] is the sub-list for method input_type
 	1, // [1:1] is the sub-list for extension type_name
@@ -307,7 +307,7 @@ const _ = grpc.SupportPackageIsVersion6
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type PerformanceServiceClient interface {
-	GetTeamsMatchingStats(ctx context.Context, in *TeamStatRequest, opts ...grpc.CallOption) (*TeamStatResponse, error)
+	GetTeamsMatchingStat(ctx context.Context, in *TeamStatRequest, opts ...grpc.CallOption) (*TeamStatResponse, error)
 }
 
 type performanceServiceClient struct {
@@ -318,9 +318,9 @@ func NewPerformanceServiceClient(cc grpc.ClientConnInterface) PerformanceService
 	return &performanceServiceClient{cc}
 }
 
-func (c *performanceServiceClient) GetTeamsMatchingStats(ctx context.Context, in *TeamStatRequest, opts ...grpc.CallOption) (*TeamStatResponse, error) {
+func (c *performanceServiceClient) GetTeamsMatchingStat(ctx context.Context, in *TeamStatRequest, opts ...grpc.CallOption) (*TeamStatResponse, error) {
 	out := new(TeamStatResponse)
-	err := c.cc.Invoke(ctx, "/proto.PerformanceService/GetTeamsMatchingStats", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/proto.PerformanceService/GetTeamsMatchingStat", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -329,35 +329,35 @@ func (c *performanceServiceClient) GetTeamsMatchingStats(ctx context.Context, in
 
 // PerformanceServiceServer is the server API for PerformanceService service.
 type PerformanceServiceServer interface {
-	GetTeamsMatchingStats(context.Context, *TeamStatRequest) (*TeamStatResponse, error)
+	GetTeamsMatchingStat(context.Context, *TeamStatRequest) (*TeamStatResponse, error)
 }
 
 // UnimplementedPerformanceServiceServer can be embedded to have forward compatible implementations.
 type UnimplementedPerformanceServiceServer struct {
 }
 
-func (*UnimplementedPerformanceServiceServer) GetTeamsMatchingStats(context.Context, *TeamStatRequest) (*TeamStatResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetTeamsMatchingStats not implemented")
+func (*UnimplementedPerformanceServiceServer) GetTeamsMatchingStat(context.Context, *TeamStatRequest) (*TeamStatResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetTeamsMatchingStat not implemented")
 }
 
 func RegisterPerformanceServiceServer(s *grpc.Server, srv PerformanceServiceServer) {
 	s.RegisterService(&_PerformanceService_serviceDesc, srv)
 }
 
-func _PerformanceService_GetTeamsMatchingStats_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _PerformanceService_GetTeamsMatchingStat_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(TeamStatRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(PerformanceServiceServer).GetTeamsMatchingStats(ctx, in)
+		return srv.(PerformanceServiceServer).GetTeamsMatchingStat(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/proto.PerformanceService/GetTeamsMatchingStats",
+		FullMethod: "/proto.PerformanceService/GetTeamsMatchingStat",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PerformanceServiceServer).GetTeamsMatchingStats(ctx, req.(*TeamStatRequest))
+		return srv.(PerformanceServiceServer).GetTeamsMatchingStat(ctx, req.(*TeamStatRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -367,8 +367,8 @@ var _PerformanceService_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*PerformanceServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "GetTeamsMatchingStats",
-			Handler:    _PerformanceService_GetTeamsMatchingStats_Handler,
+			MethodName: "GetTeamsMatchingStat",
+			Handler:    _PerformanceService_GetTeamsMatchingStat_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
