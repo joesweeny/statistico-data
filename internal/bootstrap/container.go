3283af4dbdf16db782e2abb6437db349a8047ef4
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-01-14 17:26:04 +0000

    Adjust client timeout settings

--- internal/bootstrap/container.go
+++ internal/bootstrap/container.go
@@ -66,7 +66,7 @@ func sportMonksClient(config *Config) *spClient.HTTPClient {
 		Dial: (&net.Dialer{
 			Timeout: 10 * time.Second,
 		}).Dial,
-		TLSHandshakeTimeout: 15 * time.Second,
+		TLSHandshakeTimeout: 30 * time.Second,
 	}
 
 	client := &http.Client{
